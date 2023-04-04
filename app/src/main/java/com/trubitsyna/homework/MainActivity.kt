e5d3242f3c6ca2f003aac60fffd5023f2e3eab26
julia <y.trubitsyna@g.nsu.ru>
2023-04-04 17:40:47 +0700

    lesson6

--- app/src/main/java/com/trubitsyna/homework/MainActivity.kt
+++ app/src/main/java/com/trubitsyna/homework/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.trubitsyna.homework
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.trubitsyna.homework.databinding.ActivityMainBinding
+import com.trubitsyna.homework.second_activity.SecondActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    private val listAdapter by lazy {
-        DataListAdapter()
-    }
-
-    private val dataList = mutableListOf<DataModel>(
-        DataModel(title = "Title 1", subtitle = "Subtitle 1"),
-        DataModel(title = "Title 2", subtitle = "Subtitle 2"),
-        DataModel(title = "Title 3", subtitle = "Subtitle 3"),
-        DataModel(title = "Title 4", subtitle = "Subtitle 4")
-    )
 
     override fun onCreate(
         savedInstanceState: Bundle?
@@ -26,20 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.buttonAdd.setOnClickListener {
-            dataList.add(DataModel(title = "Title 4", subtitle = "Subtitle 4"))
-            listAdapter.submitList(dataList.toList())
+        binding.buttonAction.setOnClickListener{
+            val text = binding.editText.text.toString()
+            startActivity(SecondActivity.createIntent(this, text))
         }
-
-        binding.buttonDelete.setOnClickListener {
-            dataList.removeLastOrNull()
-            listAdapter.submitList(dataList.toList())
-        }
-        listAdapter.setCallback {model ->
-            Toast.makeText(this, model.title, Toast.LENGTH_SHORT ).show()
-        }
-        binding.recyclerView.adapter = listAdapter
-
     }
 }
